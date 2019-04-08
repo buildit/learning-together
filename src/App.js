28e6ae5d7607b636d20a632b5cf5d507b91a90b0
Mabel Hernandez <mabel.hernandez@wipro.com>
2019-04-08 13:40:05 -0400

    setting up landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Jumbotron from "./components/jumbotron/jumbotron";
+import Preview from "./components/preview/preview";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workshops: [
+        {
+          id: "1",
+          name: "Redux",
+          date: "March 1",
+          description: "Learn redux techniques",
+          instructor: "John Smith",
+          location: "Brooklyn",
+          time: "1400",
+          image: "https://via.placeholder.com/40",
+          image_desc: "course image",
+          category: "1"
+        },
+        {
+          id: "2",
+          name: "React",
+          date: "March 16",
+          description: "Learn react techniques",
+          instructor: "Jasmin Smith",
+          location: "Brooklyn",
+          time: "1200",
+          image: "https://via.placeholder.com/40",
+          image_desc: "course image",
+          category: "1"
+        },
+        {
+          id: "3",
+          name: "Saga",
+          description: "Learn saga techniques",
+          instructor: "Jane Smith",
+          location: "Brooklyn",
+          time: "1000",
+          image: "https://via.placeholder.com/40",
+          image_desc: "course image",
+          category: "2"
+        },
+        {
+          id: "4",
+          name: "Yoga",
+          description: "Learn yoga techniques",
+          instructor: "Jazmin Smith",
+          location: "Brooklyn",
+          time: "1400",
+          image: "https://via.placeholder.com/40",
+          image_desc: "course image",
+          category: "3"
+        }
+      ]
+    };
+  }
+
   render() {
+    const wrkshopPreview = this.state.workshops.map(workshop => {
+      return <Preview key={workshop.id} workshop={workshop} />;
+    });
+
     return (
       <div className="App">
-        <h1>Learning Together</h1>
+        <Navbar />
+        <Jumbotron />
+        {wrkshopPreview}
       </div>
     );
   }
