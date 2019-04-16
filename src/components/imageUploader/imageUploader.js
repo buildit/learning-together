06baac096a0ce3a24be60ec2d84f681094538448
James Muller <>
2019-04-16 16:36:37 -0400

    added check mark for successful upload

--- src/components/imageUploader/imageUploader.js
+++ src/components/imageUploader/imageUploader.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { uploadImage } from '../../api'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './imageUploader.scss'
 export default class ImageUploaderComponent extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { selectedFile: '', previewImg: 'https://dummyimage.com/640x360/fff/aaa' }
+    this.state = { selectedFile: '', previewImg: 'https://dummyimage.com/300x300/fff/aaa', isUploaded: false }
     this.fileUploadCallback = this.fileUploadCallback.bind(this)
   }
 
@@ -22,21 +23,43 @@ export default class ImageUploaderComponent extends React.Component {
   }
 
   fileUploadCallback(response) {
-    this.props.setPicture(response.data)
+    if (response.data) {
+      this.setState({ isUploaded: true })
+      this.props.setPicture(response.data)
+    } else {
+      this.setState({ isUploaded: false })
+      console.log(response)
+    }
   }
   render() {
+    const { previewImg, isUploaded } = this.state
     return (
       <div className='grid-container'>
         <div className="grid-y medium-grid-frame">
-          <div className="grid-x grid-padding-x align-center">
-            <div className='image-container'>
-              <img src={this.state.previewImg} className='cell medium-12' accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|images/*" alt='preview' height='100%' width='100%' />
+          <div className='row'>
+            <div className="grid-x grid-padding-x align-center align-middle">
+              <div className='image-container'>
+                <img src={previewImg} className='cell medium-12' accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|images/*" alt='preview' height='100%' width='100%' />
+              </div>
             </div>
-            <label htmlFor="fileUpload" className="button">Choose File</label>
-            <input type='file' id='fileUpload' className='show-for-sr' onChange={this.fileUploadHandler.bind(this)} />
-            <button onClick={this.fileUploadSubmitHandler.bind(this)} className='button success'>Upload</button>
           </div>
-
+          <div className='row'>
+            <div className='grid-x grid-padding-x align-center'>
+              <label htmlFor="fileUpload" className="button">Choose File</label>
+              <input type='file' id='fileUpload' className='show-for-sr' onChange={this.fileUploadHandler.bind(this)} />
+              <button onClick={this.fileUploadSubmitHandler.bind(this)} className='button success'>Upload</button>
+              {isUploaded && (
+                <div className='image-icon-confirm'>
+                  <FontAwesomeIcon icon='check'></FontAwesomeIcon>
+                </div>
+              )}
+              {!isUploaded && (
+                <div className='image-icon-confirm'>
+                  <FontAwesomeIcon icon='minus'></FontAwesomeIcon>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div >
 
