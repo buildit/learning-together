a01a71f6c2ab73c8244a4312031d417f2d6d473b
Cecilia Avery <cecilia@Cecilias-MacBook-Pro.local>
2019-04-18 14:41:13 -0400

    bug-fix-workshop-routing

--- src/components/workshopPreview/workshopPreview.js
+++ src/components/workshopPreview/workshopPreview.js
@@ -5,10 +5,11 @@ import Moment from 'react-moment';
 import "./workshopPreview.scss";
 
 const WorkshopPreview = ({ workshop }) => {
+  console.log('workshop id', workshop)
   return (
 
     <article className="workshop grid-container">
-      <Link to={`/workshop/${workshop.workshopId}`} className="workshop-link grid-x grid-margin-x">
+      <Link to={`/workshop/${workshop.workshopId ? workshop.workshopId : workshop.id}`} className="workshop-link grid-x grid-margin-x">
         <div className="cell small-12 cell">
           <p className="from-now"><Moment format="dddd">{workshop.startDate}</Moment> <Moment format="LT">{workshop.startDate}</Moment> | <Moment fromNow>{workshop.startDate}</Moment></p>
         </div>
