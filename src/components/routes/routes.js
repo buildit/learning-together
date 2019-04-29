2102d69ba634e6ff1a69de34d4bb367da34bdac8
James Muller <>
2019-04-29 16:20:57 -0400

    removed forgot password screen

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -24,7 +24,6 @@ export default class RoutesComponent extends React.Component {
               <UserRoute exact path="/" component={LandingComponent} />
               <UnauthenticatedUserRoute exact path="/login" component={LoginComponent} />
               <UnauthenticatedUserRoute exact path="/register" component={RegisterComponent} />
-              <UnauthenticatedUserRoute exact path="/forgot-password" component={ForgotPasswordComponent} />
               <UserRoute exact path="/user/:id" component={UserProfileComponent} />
               <UserRoute path="/workshops/categories/:id/:title" component={WorkshopListComponent} />
               <UserRoute exact path="/workshop/:id" component={WorkshopComponent} />
