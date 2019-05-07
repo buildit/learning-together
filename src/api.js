1e42b4d50802bcf2ad488610f60bca6e58a032d7
James Muller <>
2019-05-07 16:23:16 -0400

    added user edit api

--- src/api.js
+++ src/api.js
@@ -106,6 +106,28 @@ export const getUser = id => {
   });
 };
 
+export const editUser = ({ firstName, lastName, username, password, roleId, locationId, imageUrl }, id) => {
+  const data = { firstName, lastName, username, password, roleId, locationId, imageUrl }
+  return axios
+    .request({
+      url:
+        `https://bettertogether.buildit.systems/api/users/${id}`,
+      method: "put",
+      data,
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      return response;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
 // Make a request for a user with a given token
 export const createWorkshop = data => {
   return axios
@@ -118,12 +140,12 @@ export const createWorkshop = data => {
         Authorization: "Bearer " + token
       }
     })
-    .then(function(response) {
+    .then(function (response) {
       // handle success
       console.log(response);
       return response;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // handle error
       console.log(error);
     });
@@ -201,11 +223,11 @@ export const updateWorkshop = (id, data) => {
         Authorization: "Bearer " + token
       }
     })
-    .then(function(response) {
+    .then(function (response) {
       // handle success
       return response;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // handle error
       console.log(error);
     });
@@ -218,14 +240,14 @@ export const fetchWorkshops = () => {
         "https://bettertogether.buildit.systems/api/workshops",
       method: "get"
     })
-    .then(function(response) {
+    .then(function (response) {
       // handle success
       if (response.data && response.status === 200) {
         //console.log("response", response);
         return response.data;
       }
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // handle error
       console.log(error);
     });
@@ -241,10 +263,10 @@ export const cancelWorkshop = (id, callback) => {
         Authorization: "Bearer " + token
       }
     })
-    .then(function(response) {
+    .then(function (response) {
       callback(response);
     })
-    .catch(function(error) {
+    .catch(function (error) {
       callback(error);
     });
 };
