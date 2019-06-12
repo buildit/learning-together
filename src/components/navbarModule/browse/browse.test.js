8902738961745919a854bf1b14a314fa417fc54e
James Muller <>
2019-06-12 11:11:37 -0400

    updated browse test

--- src/components/navbarModule/browse/browse.test.js
+++ src/components/navbarModule/browse/browse.test.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import { default as Component } from './browse'
 import { shallow } from 'enzyme';
-import { WorkshopPreviewComponent, SortComponent } from "../../workshopModule";
+import { CategoryListComponent, } from "../../categoryModule";
 import { NavbarComponent } from "../navbar";
 
 describe('>>> BrowseComponent', () => {
-  describe('rendering navbar and sort component', () => {
-    it('should render navbar component', () => {
+  describe('rendering caetgorylist component', () => {
+    it('should render categoryList component', () => {
       const wrapper = shallow(<Component />)
-      expect(wrapper.find(NavbarComponent).length).toEqual(1)
-    })
-    it('should render navbar component', () => {
-      const wrapper = shallow(<Component />)
-      expect(wrapper.find(SortComponent).length).toEqual(1)
-    })
-  })
-  describe('render WorkshopPreviewComponent based on how many instances in array', () => {
-    it('should render 3 WorkshopPreviewComponents', () => {
-      const wrapper = shallow(<Component />)
-      wrapper.setState({ workshops: [{ id: 1 }, { id: 2 }, { id: 3 }] })
-      expect(wrapper.find(WorkshopPreviewComponent).length).toEqual(3)
+      expect(wrapper.find(CategoryListComponent).length).toEqual(1)
     })
   })
 })
