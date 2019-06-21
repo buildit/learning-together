a22df10f7988740a41260212c4bf3c7fb82b4c7f
James Muller <>
2019-06-21 15:09:25 -0400

    added login cleanup

--- src/components/authModule/userRoute/userRoute.js
+++ src/components/authModule/userRoute/userRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 const UserRoute = ({ component: Component, ...props }) => {
-  if (localStorage.getItem('username')) {
+  if (localStorage.getItem('username') && sessionStorage.getItem('msal.nonce.idtoken')) {
     return (
       <Route {...props} render={() => <Component {...props} />} />
     );
