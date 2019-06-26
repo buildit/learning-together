e1524b4a98c29a9500313f117c133797d46f94a4
James Muller <>
2019-06-26 15:25:57 -0400

    cahnged the confirm message on button to OK

--- src/components/messageModule/message/message.js
+++ src/components/messageModule/message/message.js
@@ -15,7 +15,7 @@ export default class MessageComponent extends React.Component {
           <div className='message-content'>
             {this.props.message}
           </div>
-          <button type='button' className='success button' onClick={this.onClickCallback.bind(this)} >Confirm</button>
+          <button type='button' className='success button' onClick={this.onClickCallback.bind(this)} >OK</button>
         </div>
       </div>
     )
