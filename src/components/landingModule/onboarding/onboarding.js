363ea3b0458672fba8739705b4a26749e15e2ed9
Tanya Yarmolik <tanyayarmolik@c02xt2pqjgh5.home>
2019-07-16 10:35:24 -0400

    fixed user id

--- src/components/landingModule/onboarding/onboarding.js
+++ src/components/landingModule/onboarding/onboarding.js
@@ -5,25 +5,25 @@ import './onboarding.scss';
 
 export default class Onboarding extends Component {
     render() {
-        if (this.props.user && this.props.user.userInterests.length === 0) {
-            return (
-                <section className="grid-x complete-profile-callout">
-                    <div className="cell medium-6">
-                        <h3 className="greeting">Welcome {this.props.user.firstName}!</h3>
-                        <p>
-                            <b>Better Together </b>
-                            is a platform built to help Buildit and Designit employees
-                            share knowledge by creating and attending workshops. Help us to recomend workshops that may interest you by adding your office location and subjects that interest you to your profile.
-                                               </p>
-                    </div>
-                    <div className="cell medium-6 flex-container align-self-middle">
-                        <button className="button call-to-action complete-profile">
-                            <Link to={`/settings/${this.props.user.userId}`} style={{ color: "white" }}>Complete Your Profile</Link>
-                        </button>
-                    </div>
-                </section>
-            )
-        }
-        return null
+      if (this.props.user && this.props.user.userInterests.length === 0) {
+        return (
+          <section className="grid-x complete-profile-callout">
+            <div className="cell medium-6">
+              <h3 className="greeting">Welcome {this.props.user.firstName}!</h3>
+                <p>
+                  <b>Better Together </b>
+                    is a platform built to help Buildit and Designit employees
+                    share knowledge by creating and attending workshops. Help us to recomend workshops that may interest you by adding your office location and subjects that interest you to your profile.
+                </p>
+            </div>
+            <div className="cell medium-6 flex-container align-self-middle">
+              <button className="button call-to-action complete-profile">
+                <Link to={`/settings/${this.props.user.id}`} style={{ color: "white" }}>Complete Your Profile</Link>
+              </button>
+            </div>
+          </section>
+        )
+      }
+      return null
     }
 }
