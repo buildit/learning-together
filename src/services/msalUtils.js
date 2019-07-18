dc33d8a39f869869cff8a9fff739649a766d7574
James Muller <>
2019-07-18 13:38:42 -0400

    changed scopes

--- src/services/msalUtils.js
+++ src/services/msalUtils.js
@@ -10,7 +10,7 @@ export function login() {
       "Mail.Send.Shared"
     ]
   }
-  window.msal.loginPopup(request)
+  window.msal.loginPopup(request.scopes)
     .then(response => {
       loginCallback(response)
     })
