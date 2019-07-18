06374972d7279bc49001b1e4915ecaeb5a86c82b
James Muller <>
2019-07-18 15:01:23 -0400

    reverted changes back

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMapMarker, faUserCircle, faPencilAlt, faSearch, faVideo, faBuilding, faClock, faSpinner, faCheck, faMinus } from "@fortawesome/free-solid-svg-icons";
 import './App.scss'
 import { UserAgentApplication } from 'msal'
+import config from './services/config'
 
 library.add(faMapMarker, faUserCircle, faPencilAlt, faSearch, faVideo, faBuilding, faClock, faSpinner, faCheck, faMinus);
 class App extends Component {
   constructor() {
     super()
-    this.userAgentApplication = new UserAgentApplication({ auth: { clientId: 'fd8a25a4-d4ae-4ec9-96e7-bec62ae45ca8' } })
+    this.userAgentApplication = new UserAgentApplication(config.msalConfig)
   }
   render() {
     return (
